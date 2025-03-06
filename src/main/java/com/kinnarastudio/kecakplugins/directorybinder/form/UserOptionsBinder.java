39f09c0f227342d2ad37bb6804f93c581cf925b2
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2025-03-06 10:49:04 +0700

    remove logs

--- src/main/java/com/kinnarastudio/kecakplugins/directorybinder/form/UserOptionsBinder.java
+++ src/main/java/com/kinnarastudio/kecakplugins/directorybinder/form/UserOptionsBinder.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.kinnarastudio.kecakplugins.directorybinder.form;
 
 import com.kinnarastudio.commons.Try;
@@ -26,7 +21,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Stream;
 
 /**
  *
